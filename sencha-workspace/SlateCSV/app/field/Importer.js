5c121899d9a3d60f882079a353840505df13ae3e
Bill Clinton <BillClinton@users.noreply.github.com>
2015-06-21 05:49:08 -0400

    tabs converted to spaces (:retab)

--- sencha-workspace/SlateCSV/app/field/Importer.js
+++ sencha-workspace/SlateCSV/app/field/Importer.js
@@ -1,18 +1,18 @@
 /*jslint browser: true, undef: true, laxcomma:true *//*global Ext*/
 Ext.define('SlateCSV.field.Importer', {
-	extend: 'Ext.form.field.ComboBox',
-	xtype: 'slatecsv-importerfield',
+    extend: 'Ext.form.field.ComboBox',
+    xtype: 'slatecsv-importerfield',
 
     // TODO: style this with sass rather than using widths here?
     width: 400,
     labelWidth: 160,
 
-	forceSelection: true,
-	editable: false,
-	autoSelect: true,
-	labelAlign: 'right',
-	labelSeparator: ' ↔ ',
-	displayField: 'label',
-	valueField: 'id',
-	valueNotFoundText: 'None'
+    forceSelection: true,
+    editable: false,
+    autoSelect: true,
+    labelAlign: 'right',
+    labelSeparator: ' ↔ ',
+    displayField: 'label',
+    valueField: 'id',
+    valueNotFoundText: 'None'
 });
