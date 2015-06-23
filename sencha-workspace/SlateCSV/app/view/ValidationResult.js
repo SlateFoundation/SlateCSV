de07c87138f930320babb3e0240499580f6a72d2
Bill Clinton <BillClinton@users.noreply.github.com>
2015-06-23 01:29:08 -0400

    put in card layout for easy switching to an "importing..." message

--- sencha-workspace/SlateCSV/app/view/ValidationResult.js
+++ sencha-workspace/SlateCSV/app/view/ValidationResult.js
@@ -4,40 +4,47 @@ Ext.define('SlateCSV.view.ValidationResult', {
     xtype: 'slatecsv-view-validationresult',
 
     config: {
-        bodyPadding: 12,
-        tpl: [
-            '<table>',
-            // TODO: remove total failures... using it for debugging
-            //'<tr><td colspan="2">Total Fails: {failures}</td></tr>',
-            '<tpl if="failures &gt; 0">',
-                '<tpl for="validations">',
-                    '<tpl if="invalidRows.length &gt; 0">',
-                        '<tr>',
-                        '<td>Mapping:</td><td>{label}  ↔  {fieldLabel}</td>',
-                        '</tr>',
-                        '<tr>',
-                        '<td>Warning:</td><td>{vtext}</td>',
-                        '</tr>',
-                        '<tr>',
-                        '<td>Rows:</td><td>{[this.lineCount(values.invalidRows)]}</td>',
-                        '</tr>',
-                        '<tr><td colspan="2">&nbsp</td></tr>',
+        layout: 'card',
+        items: [{
+            bodyPadding: 12,
+            itemId: 'validation-summary',
+            tpl: [
+                '<table>',
+                // TODO: remove total failures... using it for debugging
+                //'<tr><td colspan="2">Total Fails: {failures}</td></tr>',
+                '<tpl if="failures &gt; 0">',
+                    '<tpl for="validations">',
+                        '<tpl if="invalidRows.length &gt; 0">',
+                            '<tr>',
+                            '<td>Mapping:</td><td>{label}  ↔  {fieldLabel}</td>',
+                            '</tr>',
+                            '<tr>',
+                            '<td>Warning:</td><td>{vtext}</td>',
+                            '</tr>',
+                            '<tr>',
+                            '<td>Rows:</td><td>{[this.lineCount(values.invalidRows)]}</td>',
+                            '</tr>',
+                            '<tr><td colspan="2">&nbsp</td></tr>',
+                        '</tpl>',
                     '</tpl>',
+                '<tpl else>',
+                    '<tr><td colspan="2">All validations passed</td></tr>',
                 '</tpl>',
-            '<tpl else>',
-                '<tr><td colspan="2">All validations passed</td></tr>',
-            '</tpl>',
-            '</table>',
-        {
-            disableFormats: true,
-            lineCount: function(lines){
-                if (lines.length > 10) {
-                    return 'occurs on '+lines.length+' rows';
-                } else {
-                    return lines.join();
-                }
+                '</table>',
+            {
+                disableFormats: true,
+                lineCount: function(lines){
+                    if (lines.length > 10) {
+                        return 'occurs on '+lines.length+' rows';
+                    } else {
+                        return lines.join();
+                    }
 
-            }
+                }
+            }]
+        },{
+            xtype: 'container',
+            html: 'importing data...'
         }]
     }
 });
